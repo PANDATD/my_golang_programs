5694642b4a379bf74dd81de0d3d5511c1241ec6d
TEJAS DIXIT <tejasdixit17@gmail.com>
2021-01-21 17:26:46 +0530

    Hello World in go lang

---
+++ hello_world.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		myName string = "Tejas Dixit"
+		age    int    = 20
+		email  string = "tejasdixit17@gmail.com"
+	)
+	fmt.Println(" Welcome", myName, "\n",
+		"age:", age, "\n",
+		"email:", email)
+
+	salary := 200000.0020
+	languge := "Python"
+	fmt.Println(salary)
+	fmt.Println(languge)
+
+}
